4718d049e0ea56fc10f4a49a88d4fae684c1a8fe
Youngnamez <etienneh78@gmail.com>
2021-06-19 11:37:51 -0400

    Forgot to push files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
@@ -22,7 +21,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader,  'css-loader', 'sass-loader' ]
+                use: ['css-loader', 'sass-loader', 'style-loader']
             }
         ]
     },
@@ -31,7 +30,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css" }),
         new WorkboxPlugin.GenerateSW()
     ],
     optimization: {
